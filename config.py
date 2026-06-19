bb1d0078fb600779a698f7c7f5cbdc5db81dac13
william <wcortez799@hotmail.com>
2026-06-18 23:02:26 -0600

    issue con recuperar password modificar config.py 02

--- config.py
+++ config.py
@@ -17,14 +17,17 @@ os.makedirs(DATABASE_DIR, exist_ok=True)
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
 
-    MAIL_SERVER = "smtp.gmail.com"
-    MAIL_PORT = 465
-    MAIL_USE_TLS = False
-    MAIL_USE_SSL = True
+    MAIL_SERVER = "smtp-relay.brevo.com"
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
+
     MAIL_USERNAME = os.getenv("MAIL_USERNAME")
     MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
+
     MAIL_DEFAULT_SENDER = os.getenv("MAIL_USERNAME")
-    MAIL_TIMEOUT = 10
+
+    MAIL_TIMEOUT = 20
     
     DATABASE_URL = os.getenv("DATABASE_URL")
 
