57f5051ce6e7ed58ef4d8d44ac4fb227fbab1b3a
william <wcortez799@hotmail.com>
2026-06-18 21:26:32 -0600

    issue con recuperar password modificar config.py

--- config.py
+++ config.py
@@ -18,8 +18,9 @@ class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
 
     MAIL_SERVER = "smtp.gmail.com"
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
     MAIL_USERNAME = os.getenv("MAIL_USERNAME")
     MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
     MAIL_DEFAULT_SENDER = os.getenv("MAIL_USERNAME")
