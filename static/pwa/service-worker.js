6c5459179d3c283c64f21c2134b9f4e0072d712b
william <wcortez799@hotmail.com>
2026-06-24 19:08:43 -0600

    PWA

--- static/pwa/service-worker.js
+++ static/pwa/service-worker.js
@@ -1,39 +1,29 @@
-const CACHE_NAME = "control-gastos-v1";
-
-const urlsToCache = [
-    "/",
-    "/dashboard",
-    "/static/css/style.css"
-];
+const CACHE_NAME = "control-gastos-v2";
 
 self.addEventListener("install", event => {
+    self.skipWaiting();
+});
 
+self.addEventListener("activate", event => {
     event.waitUntil(
-
-        caches.open(CACHE_NAME)
-
-        .then(cache => {
-
-            return cache.addAll(urlsToCache);
-
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
         })
-
     );
 
+    self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
-
     event.respondWith(
-
-        caches.match(event.request)
-
-        .then(response => {
-
-            return response || fetch(event.request);
-
+        fetch(event.request).catch(() => {
+            return caches.match(event.request);
         })
-
     );
-
 });
