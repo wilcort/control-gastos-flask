db46f0308b66b8130d0dbcce191722d830ed77a9
william <wcortez799@hotmail.com>
2026-06-28 19:31:52 -0600

    Improve PWA configuration

--- static/pwa/service-worker.js
+++ static/pwa/service-worker.js
@@ -1,6 +1,23 @@
-const CACHE_NAME = "control-gastos-v2";
+const CACHE_NAME = "control-gastos-v3";
+
+const STATIC_ASSETS = [
+    "/",
+    "/login",
+    "/register",
+    "/static/pwa/manifest.json",
+    "/static/pwa/icon-192.png",
+    "/static/pwa/icon-512.png",
+    "/static/pwa/maskable-icon-192.png",
+    "/static/pwa/maskable-icon-512.png"
+];
 
 self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(STATIC_ASSETS);
+        })
+    );
+
     self.skipWaiting();
 });
 
@@ -21,9 +38,23 @@ self.addEventListener("activate", event => {
 });
 
 self.addEventListener("fetch", event => {
+    if (event.request.method !== "GET") {
+        return;
+    }
+
     event.respondWith(
-        fetch(event.request).catch(() => {
-            return caches.match(event.request);
-        })
+        fetch(event.request)
+            .then(response => {
+                const responseClone = response.clone();
+
+                caches.open(CACHE_NAME).then(cache => {
+                    cache.put(event.request, responseClone);
+                });
+
+                return response;
+            })
+            .catch(() => {
+                return caches.match(event.request);
+            })
     );
 });
